3edf040a1e72def6d2e9a73c2efe3cc8a999894b
drselump14 <botoksgonzales@gmail.com>
2017-09-08 09:07:08 +0900

    update for swift3 compatible

--- Classes/XXPagingScrollView.swift
+++ Classes/XXPagingScrollView.swift
@@ -13,7 +13,7 @@ public class XXPagingScrollView: UIView {
 
     private var widthContraint:Constraint? = nil
     private var heightContraint:Constraint? = nil
-    
+
     /// 0 means regular paging mode (as long as self)
     public var pagingWidth:CGFloat = 0 {
         didSet {
@@ -22,13 +22,13 @@ public class XXPagingScrollView: UIView {
             }
             else {
                 widthContraint?.deactivate()
-                self.scrollView.snp_updateConstraints{ (make) -> Void in
+                self.scrollView.snp.makeConstraints({ (make) -> Void in
                     make.width.equalTo(self.pagingWidth)
-                }
+                })
             }
         }
     }
-    
+
     /// 0 means regular paging mode (as long as self)
     public var pagingHeight:CGFloat = 0 {
         didSet {
@@ -37,49 +37,48 @@ public class XXPagingScrollView: UIView {
             }
             else {
                 heightContraint?.deactivate()
-                self.scrollView.snp_updateConstraints{ (make) -> Void in
+                self.scrollView.snp.makeConstraints({ (make) -> Void in
                     make.height.equalTo(self.pagingHeight)
-                }
+                })
             }
         }
     }
-    
+
     private class XXReachableScrollView:UIScrollView {
-        
-        override func pointInside(point: CGPoint, withEvent event: UIEvent?) -> Bool {
+
+        override func point(inside: CGPoint, with event: UIEvent?) -> Bool {
             return true
         }
-        
+
     }
-    
+
     public lazy var scrollView:UIScrollView! = {
         var v = XXReachableScrollView()
         v.clipsToBounds = false
-        v.pagingEnabled = true
+        v.isPagingEnabled = true
         v.showsVerticalScrollIndicator = false
         v.showsHorizontalScrollIndicator = false
         return v
         }()
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         self.setup()
     }
-    
+
     public required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)!
-        self.setup()
+        fatalError("init(coder: ) has not been implemented")
     }
-    
+
     private func setup() {
-        
+
         self.addSubview(self.scrollView)
-        self.scrollView.snp_makeConstraints { (make) -> Void in
+        self.scrollView.snp.makeConstraints { (make) -> Void in
             make.center.equalTo(self)
-            make.width.equalTo(self.pagingWidth).priorityLow()
-            make.height.equalTo(self.pagingHeight).priorityLow()
-            self.widthContraint = make.width.equalTo(self.snp_width).priorityHigh().constraint
-            self.heightContraint = make.height.equalTo(self.snp_height).priorityHigh().constraint
+            make.width.equalTo(self.pagingWidth).priority(.low)
+            make.height.equalTo(self.pagingHeight).priority(.low)
+            self.widthContraint = make.width.equalTo(self.snp.width).priority(.high).constraint
+            self.heightContraint = make.height.equalTo(self.snp.height).priority(.high).constraint
         }
     }
 
